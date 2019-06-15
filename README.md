ba2a22d1edd38a2bc6a24f97fed771f1149be360
Matt Zizzi <mzizzi@bandwidth.com>
2019-06-15 19:21:16 -0400

    tutorial

--- README.md
+++ README.md
@@ -1,8 +1,93 @@
 # DataDog Operator
 
-Manage [Datadog|](https://www.datadoghq.com/) resources from within Kubernetes.
+Manage [DataDog](https://www.datadoghq.com/) resources from within Kubernetes. Currently only
+supports DataDog Monitors.
 
-**Warning: Code is littered with TODOs.**
+**Warning:** The code is littered with TODOs and should be considered for demonstration purposes
+only. There are a lot of edge cases that aren't accounted for that will lead to endless event
+processing loops in your cluster and orphaned resources in your DataDog account.
 
 Built using [Kubernetes Operator Pythonic Framework](https://github.com/zalando-incubator/kopf)
 
+## Custom Resource Definitions
+
+To install the CRD:
+```bash
+oc apply -f monitor-crd.yml
+```
+
+To remove the CRD:
+```bash
+oc delete crd monitors.datadog.mzizzi
+oc delete -f monitor-crd.yml
+```
+
+## Running the Operator
+
+You'll need DataDog creds. You can get them [here](https://www.datadoghq.com/free-datadog-trial/).
+You can skip setting up an agent for now. (Even though the signup process makes it seem required.)
+
+### Locally
+
+```bash
+export DATADOG_API_KEY=$myDatadogApiKey
+export DATADOG_APP_KEY=$myDatadogAppKey
+python3.7 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+kopf run datadog_operator.py
+```
+
+## Proof of Concept
+
+Assumes you're running minishift, the CRD is installed, and the operator is running.
+
+Create a DataDog monitor via a kubernetes custom resource.
+```bash
+oc apply -f examples/monitor.yml
+```
+
+Take a look at the resource in kubernetes. Note the ID of the DataDog monitor is stored in the
+status field.
+```bash
+oc get mon my-monitor -o yaml
+
+# explicitly fetching the monitor id
+oc get mon my-monitor -o json | jq .status.datadog_monitor_id
+```
+
+Check the [DataDog Manage Monitors UI](https://app.datadoghq.com/monitors/manage) and find your
+newly created monitor. Alternatively use the DataDog CLI to ensure that the monitor was really
+created over at DataDog. If you've already walked through installing everything in
+`requirements.txt` then the `dog` CLI porgram should already be available. More on configuring
+the CLI [here](https://docs.datadoghq.com/developers/guide/dogshell-asdf-use-datadog-s-api-from-terminal-shell/).
+```bash
+dog monitor show $(oc get mon my-monitor -o json | jq .status.datadog_monitor_id)
+```
+
+Let's make sure tags we set in `examples/monitor.yml` made into DataDog. Note that
+`kube_resource_id` captures the ID of the corresponding kube resource. Potentially helpful for
+identifying orphans down the road?
+```bash
+dog monitor show $(oc get mon my-monitor -o json | jq .status.datadog_monitor_id) | jq .tags
+[
+  "foo:bar",
+  "kube_resource_id:62ad94bf-8fbf-11e9-a09c-5254007ee77c"
+]
+```
+
+Replace tag `foo:bar` with `fizz:buzz`:
+```bash
+oc apply -f examples/monitor-update.yml
+dog monitor show $(oc get mon my-monitor -o json | jq .status.datadog_monitor_id) | jq .tags
+[
+  "fizz:buzz",
+  "kube_resource_id:62ad94bf-8fbf-11e9-a09c-5254007ee77c"
+]
+```
+
+Delete the monitor from kubernetes and ensure that it's deleted in DataDog:
+```bash
+oc delete mon my-monitor
+dog monitor show_all --monitor_tags=kube_resource_id:62ad94bf-8fbf-11e9-a09c-5254007ee77c
+```
--- datadog_operator.py
+++ datadog_operator.py
@@ -1,11 +1,10 @@
+import copy
 import os
 
 import kopf
 from datadog import initialize, api as datadog_api
 
-initialize(
-    api_key=os.environ.get('DATADOG_API_KEY'),
-    app_key=os.environ.get('DATADOG_APP_KEY'))
+initialize()
 
 MONITOR_ID_KEY = 'datadog_monitor_id'
 KUBE_RESOURCE_ID_KEY = 'kube_resource_id'
@@ -13,13 +12,14 @@ KUBE_RESOURCE_ID_KEY = 'kube_resource_id'
 
 @kopf.on.create('datadog.mzizzi', 'v1', 'monitors')
 def on_create(spec, patch, uid, **kwargs):
-    # Track the kubernetes resource id of this object as a tag on the monitor in
-    # datadog.
+    # TODO: should we further validate spec or let datadog kick back an error if things are off?
 
-    # TODO: re-enable this once problems described in update are solved.
-    # spec.setdefault('tags', []).append(f'{KUBE_RESOURCE_ID_KEY}:{uid}')
+    # Track the kubernetes resource id of this object as a tag on the monitor in datadog. Attempt
+    # to do this politely by making a copy of the dict.
+    request_body = copy.deepcopy(spec)
+    request_body.setdefault('tags', []).append(f'{KUBE_RESOURCE_ID_KEY}:{uid}')
 
-    response = datadog_api.Monitor.create(**spec)
+    response = datadog_api.Monitor.create(**request_body)
 
     # store the id of the datadog monitor in the kube resource's status field
     patch.setdefault('status', {})[MONITOR_ID_KEY] = response['id']
@@ -27,55 +27,54 @@ def on_create(spec, patch, uid, **kwargs):
 
 @kopf.on.update('datadog.mzizzi', 'v1', 'monitors')
 def on_update(status, spec, uid, **kwargs):
-    # TODO: maintain kubernetes resource id on the monitor as a tag in datadog
-    #  This current causes an infinite event loop in kubernetes if uncommented
-    # spec.setdefault('tags', []).append(f'{KUBE_RESOURCE_ID_KEY}:{uid}')
+    # Track the kubernetes resource id of this object as a tag on the monitor in datadog. Attempt
+    # to do this politely by making a copy of the dict.
+    request_body = copy.deepcopy(spec)
+    request_body.setdefault('tags', []).append(f'{KUBE_RESOURCE_ID_KEY}:{uid}')
 
     monitor_id = status.get(MONITOR_ID_KEY, None)
 
     if not monitor_id:
-        # TODO: status field missing. Someone(thing?) modified the resource such
-        #  that we can no longer determine what monitor to update.
-        #   * complain via somehow via kube events / status?
-        #   * create a new monitor?
-        #   * search datadog for an alarm with a matching uid?
+        # TODO: status field missing. Someone(thing?) modified the resource such that we can no
+        #  longer determine what monitor to update.
+        #    * complain via somehow via kube events / status?
+        #    * create a new monitor?
+        #    * search datadog for an alarm with a matching uid?
         pass
 
-    # update using the monitor id that's cached in this resource's status
-    # field
-    response = datadog_api.Monitor.update(status[MONITOR_ID_KEY], **spec)
+    # update using the monitor id that's cached in this resource's status field
+    response = datadog_api.Monitor.update(status[MONITOR_ID_KEY], **request_body)
 
     if 'errors' in response and response['errors']:
-        # TODO: fix the datadog SDK such that we know what type of error
-        #  occurred. As of now we don't know if the failure is "retryable" or
-        #  if it's pointless to keep trying. The datadog SDK only returns
-        #  an error object. http status code would be more helpful in
-        #  determining how to proceed.
+        # TODO: fix the datadog SDK such that we know what type of error occurred. As of now we
+        #  don't know if the failure is "retryable" or if it's pointless to keep trying. The datadog
+        #  SDK only returns an error object. http status code would be more helpful in determining
+        #  how to proceed.
         pass
+
+    return
 
 
 @kopf.on.delete('datadog.mzizzi', 'v1', 'monitors')
 def on_delete(status, spec, uid, **kwargs):
-    # TODO: figure out how kopf interacts with kubernetes finalizers on resource
-    # deletion. More info here:
+    # TODO: figure out how kopf interacts with kubernetes finalizers on resource deletion:
     # https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#finalizers
 
     monitor_id = status.get(MONITOR_ID_KEY, None)
 
     if not monitor_id:
-        # TODO: status field missing. Someone(thing?) modified the resource such
-        #  that we can no longer determine what monitor to update.
-        #   * complain via somehow via kube events / status?
-        #   * create a new monitor?
-        #   * search datadog for an alarm with a matching uid?
-        pass
+        # TODO: status field missing. Someone(thing?) modified the resource such that we can no
+        #  longer determine what monitor to update.
+        #    * complain via somehow via kube events / status?
+        #    * create a new monitor?
+        #    * search datadog for an alarm with a matching uid?
+        return
 
     response = datadog_api.Monitor.delete(monitor_id)
 
     if 'errors' in response and response['errors']:
-        # TODO: fix the datadog SDK such that we know what type of error
-        #  occurred. As of now we don't know if the failure is "retryable" or
-        #  if it's pointless to keep trying. The datadog SDK only returns
-        #  an error object. http status code would be more helpful in
+        # TODO: fix the datadog SDK such that we know what type of error occurred. As of now we
+        #  don't know if the failure is "retryable" or if it's pointless to keep trying. The
+        #  datadog SDK only returns an error object. http status code would be more helpful in
         #  determining how to proceed.
-        pass
+        return
