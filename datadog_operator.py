c69e3dec5abf5b342e733a16e7b26fb213c9d06c
Matt Zizzi <mzizzi@bandwidth.com>
2019-06-16 12:04:23 -0400

    handle monitor not found on delete

--- datadog_operator.py
+++ datadog_operator.py
@@ -1,13 +1,16 @@
 import copy
-import os
 
 import kopf
 from datadog import initialize, api as datadog_api
+from kopf import HandlerRetryError
 
 initialize()
+# datadog_api._mute = False
 
 MONITOR_ID_KEY = 'datadog_monitor_id'
 KUBE_RESOURCE_ID_KEY = 'kube_resource_id'
+
+MONITOR_NOT_FOUND = 'Monitor not found'
 
 
 @kopf.on.create('datadog.mzizzi', 'v1', 'monitors')
@@ -27,6 +30,9 @@ def on_create(spec, patch, uid, **kwargs):
 
 @kopf.on.update('datadog.mzizzi', 'v1', 'monitors')
 def on_update(status, spec, uid, **kwargs):
+    # TODO: handle case where monitor doesn't exist. Either because status[MONITOR_ID_KEY] is
+    #  missing or the monitor itself is missing on the DataDog side.
+
     # Track the kubernetes resource id of this object as a tag on the monitor in datadog. Attempt
     # to do this politely by making a copy of the dict.
     request_body = copy.deepcopy(spec)
@@ -56,10 +62,7 @@ def on_update(status, spec, uid, **kwargs):
 
 
 @kopf.on.delete('datadog.mzizzi', 'v1', 'monitors')
-def on_delete(status, spec, uid, **kwargs):
-    # TODO: figure out how kopf interacts with kubernetes finalizers on resource deletion:
-    # https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#finalizers
-
+def on_delete(status, patch, logger, **kwargs):
     monitor_id = status.get(MONITOR_ID_KEY, None)
 
     if not monitor_id:
@@ -68,6 +71,9 @@ def on_delete(status, spec, uid, **kwargs):
         #    * complain via somehow via kube events / status?
         #    * create a new monitor?
         #    * search datadog for an alarm with a matching uid?
+        logger.debug(
+            f'Monitor resource missing "{MONITOR_ID_KEY}" key. Monitor may be orphaned '
+            'in DataDog.')
         return
 
     response = datadog_api.Monitor.delete(monitor_id)
@@ -76,5 +82,11 @@ def on_delete(status, spec, uid, **kwargs):
         # TODO: fix the datadog SDK such that we know what type of error occurred. As of now we
         #  don't know if the failure is "retryable" or if it's pointless to keep trying. The
         #  datadog SDK only returns an error object. http status code would be more helpful in
-        #  determining how to proceed.
-        return
+        #  determining how to proceed. The best we cane do for now is to string compare bits and
+        #  pieces of the response until we can get hands on the underlying status code.
+        if MONITOR_NOT_FOUND in response['errors']:
+            logger.debug(f'datadog monitor {monitor_id} already deleted')
+            return
+
+        # TODO: exponential backoff
+        raise HandlerRetryError(errors=response['errors'])
